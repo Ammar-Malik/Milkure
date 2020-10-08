072801bc80e32d3aa29fa6f0016f0f0838392f73
Ammar Ejaz <52205377+Ammar-Malik@users.noreply.github.com>
2020-10-08 18:41:47 +0500

    Assignment 2 better organzied

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -3,33 +3,31 @@ import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'r
 import { Link } from 'react-router-dom';
 
 
-
-
+function RenderLeader({leader}) {
+    return(
+        <div key={leader.id} className="col-12 mt-5">
+            <Media tag="li">
+                <Media left middle>
+                    <Media object src={leader.image} alt={leader.name} />
+                </Media>
+                <Media body className="col-12">
+                    <Media heading>{leader.name}</Media>
+                    <p>{leader.designation}</p>
+                    <p>{leader.description}</p>
+                </Media>    
+            </Media>
+        </div>
+    );
+}
 
 function About(props) {
 
     const leaders = props.leaders.map((leader) => {
         return (
-            <RenderLeader leader={leader}/>
+            <RenderLeader leader={leader} />
         );
     });
-     
-    function RenderLeader({leader}){
-        return (    
-            <div key={leader.id} className="col-12 mt-5">
-                <Media tag="li">
-                  <Media left middle>
-                      <Media object src={leader.image} alt={leader.name} />
-                  </Media>
-                  <Media body className="ml-5">
-                    <Media heading>{leader.name}</Media>
-                    <p>{leader.designation}</p>
-                    <p>{leader.description}</p>
-                  </Media>
-                </Media>
-              </div>
-        );
-    }
+
     return(
         <div className="container">
             <div className="row">
@@ -94,4 +92,4 @@ function About(props) {
     );
 }
 
-export default About;  
+export default About;    
